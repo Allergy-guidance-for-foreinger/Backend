a6a8ba499be509d1cfdcfab108c62d8d31778b4a
jsj0010 <wjd020425@kumoh.ac.kr>
2026-04-04 19:48:57 +0900

    Initial commit

---
+++ src/test/java/com/mealguide/mealguide_api/MealguideApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mealguide.mealguide_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealguideApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
